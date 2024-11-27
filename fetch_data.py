380c8bd862ebbb4d4d5d62bd183966532f444552
purulek <adam.purul@gmail.com>
2024-11-27 23:54:24 +0100

    next commit

--- fetch_data.py
+++ fetch_data.py
@@ -2,3 +2,13 @@ import tweepy
 import pandas as pd
 
 
+def configure_api(api_key, api_secret, access_token, access_token_secret):
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    return api
+
+
+
+
+configure_api()

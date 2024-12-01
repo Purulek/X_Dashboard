76d02facfdf6ed5de83d81daf86eaa3d61f40e52
purulek <adam.purul@gmail.com>
2024-12-01 23:17:58 +0100

    next commit 01.12

--- fetch_data.py
+++ fetch_data.py
@@ -1,6 +1,18 @@
 import tweepy
 import pandas as pd
 
+key = 'xxxxx'
+
+secret_key = 'xxxx'
+
+Bearer_Token = 'xxxxI'
+
+acess_token = 'xxxxZ'
+
+secert_token = 'xxxxx'
+
+
+client = tweepy.Client(bearer_token=Bearer_Token)
 
 def configure_api(api_key, api_secret, access_token, access_token_secret):
     auth = tweepy.OAuthHandler(api_key, api_secret)
@@ -8,5 +20,23 @@ def configure_api(api_key, api_secret, access_token, access_token_secret):
     api = tweepy.API(auth, wait_on_rate_limit=True)
     return api
 
+def fetch_tweets(query, max_results=1):
+    response = client.search_recent_tweets(query=query, tweet_fields=['created_at', 'public_metrics'], max_results=max_results)
+    tweets = response.data
+    if not tweets:
+        return pd.DataFrame()
+    
+    data = [{
+        "text": tweet.text,
+        "created_at": tweet.created_at,
+        "retweets": tweet.public_metrics['retweet_count'],
+        "likes": tweet.public_metrics['like_count']
+    } for tweet in tweets]
+    return pd.DataFrame(data)
 
 
+
+
+
+test2 = fetch_tweets(query='#AI')
+print(test2)

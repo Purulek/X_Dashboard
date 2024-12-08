32483ea6b2860cb9eada0f4a752e9f9f02dc400c
purulek <adam.purul@gmail.com>
2024-12-08 23:05:12 +0100

    next commit

--- fetch_data.py
+++ fetch_data.py
@@ -14,11 +14,7 @@ secert_token = 'xxxx'
 
 client = tweepy.Client(bearer_token=Bearer_Token)
 
-def configure_api(api_key, api_secret, access_token, access_token_secret):
-    auth = tweepy.OAuthHandler(api_key, api_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True)
-    return api
+
 
 def fetch_tweets(query, max_results=1):
     response = client.search_recent_tweets(query=query, tweet_fields=['created_at', 'public_metrics'], max_results=max_results)
